f3c6bf296a00640d05b5d0463d642284699d007d
andre-saccoo <andre.sacco@icloud.com>
2025-11-25 13:54:54 +0100

    laboratorio

--- database/dao.py
+++ database/dao.py
@@ -1,7 +1,32 @@
 from database.DB_connect import DBConnect
+from model.hub import Hub
 
 class DAO:
-    """
-    Implementare tutte le funzioni necessarie a interrogare il database.
-    """
-    # TODO
+    """ Implementare tutte le funzioni necessarie a interrogare il database. collegare al model """
+
+    @staticmethod
+    def get_hub():
+        cnx = DBConnect.get_connection()
+
+        if cnx is None:
+            print("❌ Errore di connessione al database.")
+            return None
+
+        result = {}
+        query = " SELECT * FROM hub"
+        cursor = cnx.cursor(dictionary=True)
+
+        try:
+            cursor.execute(query)
+            for row in cursor:
+                hub=Hub(id=row[0], codice=row[1], nome=row[2], citta=row[3], stato=row[4],latitudine=row[5],longitudine=row[6])
+                result[hub.id] = hub
+
+        except Exception as e:
+            print(f"Errore durante la query get_tour: {e}")
+            result = None
+
+        finally:
+            cursor.close()
+            cnx.close()
+        return result
